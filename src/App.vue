61c544a698f865760c5f2c40b384ae2b659441fb
pramodkharade <kharade.pramod91@gmail.com>
2019-11-04 00:05:12 +0530

    default setup with  built in directives

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Built In directives</h1>
+        <p v-text="'This is some text'"></p>
+        <p v-html="'<i>This is dnyanda Kharade</i>'"></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
