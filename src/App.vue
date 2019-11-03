b91873c3ade8f84b8452a3828e756a6e94d2a2fb
pramodkharade <kharade.pramod91@gmail.com>
2019-11-04 00:23:14 +0530

    custom directive

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,14 @@
         <p v-html="'<i>This is dnyanda Kharade</i>'"></p>
       </div>
     </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom  directives</h1>
+        <p v-highlight>This is some text</p>
+
+      </div>
+    </div>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive(
+  'highlight',
+  {
+    bind(el,binding,vnode){
+      el.style.backgroundColor='green';
+    }
+  }
+);
 new Vue({
   el: '#app',
   render: h => h(App)
