bb9d5a9aed3425bf2e3012c2bf4f79d5ec086a3d
pramodkharade <kharade.pramod91@gmail.com>
2019-11-04 10:35:54 +0530

    passing more complex values to directives

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom directives</h1>
         <p v-highlight:background.delayed="'red'">This is some text</p>
-        <p v-local-highlight:background.delayed.blink="'green'">This is some text Local Directive</p>
+        <p v-local-highlight:background.delayed.blink="{mainColor:'green',secondColor:'red',delay:500}">This is some text Local Directive</p>
       </div>
     </div>
   </div>
@@ -28,8 +28,8 @@ export default {
           delay = 3000;
         }
         if (binding.modifiers["blink"]) {
-          let mainColor = binding.value;
-          let secondColor = "yellow";
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
           let currentColor = mainColor;
           setTimeout(() => {
             setInterval(() => {
@@ -41,14 +41,14 @@ export default {
               } else {
                 el.style.color = currentColor;
               }
-            }, 1000);
+            }, binding.value.delay);
           }, delay);
         } else {
           setTimeout(() => {
             if (binding.arg == "background") {
-              el.style.backgroundColor = binding.value;
+              el.style.backgroundColor = binding.value.mainColor;
             } else {
-              el.style.color = binding.value;
+              el.style.color = binding.value.mainColor;
             }
           }, delay);
         }
