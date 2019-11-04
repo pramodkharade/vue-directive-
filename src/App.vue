73a077cdc16c5ef3f8349b7757d2a42041535c5d
pramodkharade <kharade.pramod91@gmail.com>
2019-11-04 10:02:22 +0530

    registering local  directives

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Custom  directives</h1>
         <p v-highlight:background.delayed="'red'">This is some text</p>
-
+        <p v-local-highlight:background.delayed="'green'">This is some text Local Directive</p>
       </div>
     </div>
   </div>
@@ -20,7 +20,26 @@
 
 <script>
 export default {
+  directives:{
+    'local-highlight':{
+        bind(el, binding, vnode) {
 
+      let delay = 0;
+      if(binding.modifiers['delayed']){
+        delay = 3000;
+      }
+      setTimeout(()=>{
+        if (binding.arg == 'background') {
+          el.style.backgroundColor = binding.value;
+        } else {
+          el.style.color = binding.value;
+
+        }
+      },delay);
+
+    }
+    }
+  }
 }
 </script>
 
